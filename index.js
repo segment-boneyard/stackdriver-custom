783d04da42e76ca1fa5ef7694076366a9884e972
Ilya Volodarsky <ivolo@mit.edu>
2013-08-12 23:59:06 -0700

    updating API to allow for batch sending

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var _           = require('underscore'),
-    events      = require('events'),
-    util        = require('util'),
     requests    = require('request');
+
 
 /**
  *  Stackdriver node client driver
@@ -13,31 +12,16 @@ var _           = require('underscore'),
  *   @param  {String} customerId  - Stackdriver Customer Id
  *   @option {String} host        - Stackdriver API base hostname, which defaults to
  *                                  https://custom-gateway.stackdriver.com/v1/custom
- *   @param  {String} instance    - Instance ID to pass along to Stackdriver
- *   @param  {boolean} debug      - debug mode to log additional information
+ *   @param  {String} instanceId  - Instance ID to pass along to Stackdriver
+ *   @param  {String} prefix      - prefix for every metric
  */
 var Client = module.exports = function (options) {
 
-  this.initialized = false;
-
-  // No-op on error so we don't kill the server.
-  this.on('error', function () {});
-
-  if (options) this.init(options);
-};
-
-util.inherits(Client, events.EventEmitter);
-
-
-/**
- * Initializes this client.
- *
- * @param  {Object} options
- *   @param  {String} apiKey      - Stackdriver API key
- *   @param  {String} customerId  - Stackdriver Customer Id
- *   @option {String} host        - Stackdriver API base hostname
- */
-Client.prototype.init = function (options) {
+  options = _.defaults(options || {}, {
+    host       : 'https://custom-gateway.stackdriver.com/v1/custom',
+    instanceId : null,
+    prefix     : null
+  });
 
   if (!_.isString(options.apiKey) || options.apiKey.length === 0)
       throw new Error('stackdriver-custom client must be initialized with a '+
@@ -47,53 +31,20 @@ Client.prototype.init = function (options) {
       throw new Error('stackdriver-custom client must be initialized with a '+
                       'non-empty "customerId" parameter.');
 
-  this.apiKey     = options.apiKey;
-  this.customerId = options.customerId;
-  this.host       = options.host || 'https://custom-gateway.stackdriver.com/v1/custom';
-  this.debug      = options.debug || false;
-  this.instance   = options.instance;
-
-  this.initialized = true;
-
-  this.emit('initialize');
+  this.options = options;
 };
-
-
-/**
- * Internal method to check whether the client has been initialized.
- * @private
- */
-Client.prototype._checkInitialized = function () {
-  if (!this.initialized)
-    throw new Error('stackdriver-custom client is not initialized. Please call ' +
-                    'client.init(options).');
-};
-
-/**
- * Log to console if debug is enabled
- * @private
- * 
- * @param  {mixed} message whatever you want to log to console
- */
-Client.prototype._debug = function(message) {
-  if (this.debug)
-    console.log(message)
-};
-
 
 /**
  * Identifying a user ties all of their actions to an ID, and associates
  * user `traits` to that ID.
  *
  *
- * @param  {String} name        - the name of your metric
- * @param  {Number} value       - the value of your metric
- * @param  {Date}   collectedAt - the time you collected this value for the metric
+ * @param  {String} name         - the name of your metric
+ * @param  {Number} value        - the value of your metric
+ * @param  {Date}   collected_at - the time you collected this value for the metric
  *
  */
-Client.prototype.sendMetric = function (name, value, collectedAt) {
-
-  this._checkInitialized();
+Client.prototype.sendMetric = function (name, value, collected_at, callback) {
 
   if (!_.isString(name) || name.length <= 0) {
     throw new Error('[stackdriver-custom]#sendMetric: "name" must be non-empty string');
@@ -103,51 +54,42 @@ Client.prototype.sendMetric = function (name, value, collectedAt) {
     throw new Error('[stackdriver-custom]#sendMetric: "value" must be number');
   }
 
-  if (collectedAt && !_.isDate(collectedAt)) {
-    throw new Error('[stackdriver-custom]#sendMetric: "collectedAt" must be a date');
+  if (timestamp && !_.isDate(collected_at)) {
+    throw new Error('[stackdriver-custom]#sendMetric: "collected_at" must be a date');
   }
 
-  var time = (collectedAt || new Date()).getTime();
-  var timestamp = Math.round(time/1000);
+  var metric = { name: name, value: value, collected_at: collected_at };
 
-  // Create the json message body
-  var message = {
-    timestamp     : timestamp,
-    customer_id   : this.customerId,
-    proto_version : '1',
-    data : {
-        name         : name,
-        value        : value,
-        collected_at : timestamp
-    } 
-  };
+  return this.sendMetrics([metric], callback);
+};
 
-  // Add instance if provided
-  if (this.instance)
-    message.data.instance = this.instance;
 
-  // Create the headers
-  var headers = {
-    'x-stackdriver-apikey' : this.apiKey
-  };
+Client.prototype.sendMetrics = function (metrics, callback) {
 
-  // Create the full request
-  var request = {
-    url     : this.host,
-    headers : headers,
-    json    : message
-  };
+  var self = this;
 
-  // POST the message
-  this._debug(request);
-  requests.post(request, function (err, response, body) {
-    if (err)
-      throw new Error(err);
-    else if (response.statusCode !== 201) {
-      throw new Error((body && body.error && body.error.message) ||
-                          'Server error ('+response.statusCode+'): ' + JSON.stringify(body));
-    }
-    else
-      return;
+  metrics = _.map(metrics, function (metric) {
+    if (self.options.prefix)
+      metric.name = '[' + self.options.prefix + '] ' + metric.name;
+    if (self.options.instanceId)
+      metric.instance = self.options.instanceId;
+
+    metric.collected_at = Math.round(metric.collected_at.getTime() / 1000);
+    return metric;
   });
+
+  // http://feedback.stackdriver.com/knowledgebase/articles/181488-sending-custom-metrics-to-the-stackdriver-system
+  var request = {
+    url: this.options.host,
+    json: {
+      timestamp     : Math.round(Date.now() / 1000),
+      proto_version : 1,
+      customer_id   : this.options.customerId,
+      data          : metrics
+    }, headers: {
+      'x-stackdriver-apikey' : this.options.apiKey
+    }
+  };
+
+  return requests.post(request, callback);
 };
--- test.js
+++
@@ -1,8 +0,0 @@
-var Stackdriver = require('./index.js');
-
-var client = new Stackdriver({
-    apiKey     : 'x',
-    customerId : 1
-});
-
-client.sendMetric('test metric a123', 123);
